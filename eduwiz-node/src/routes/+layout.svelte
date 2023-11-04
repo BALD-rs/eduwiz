c34bacf3fe31ea5e976c2f723dee3db8cf6569ba
Blaine Traudt <blaine-t@bathost.net>
2023-11-04 10:06:04 -0500

    Attempt to standarize css

--- eduwiz-node/src/routes/+layout.svelte
+++ eduwiz-node/src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <svelte:head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 	<link rel="manifest" href="/site.webmanifest" />
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2984e7" />
+
 	<meta name="apple-mobile-web-app-title" content="eduwiz" />
 	<meta name="application-name" content="eduwiz" />
 	<meta name="msapplication-TileColor" content="#efefef" />
 	<meta name="theme-color" content="#efefef" />
+
+	<link rel="stylesheet" type="text/css" href="styles.css" />
 </svelte:head>
 
 <slot />
--- eduwiz-node/src/routes/+page.svelte
+++ eduwiz-node/src/routes/+page.svelte
@@ -1,57 +1,26 @@
 <script>
-  import { onMount } from 'svelte';
-  import { goto } from '$app/navigation';
-  import { logIn, user } from '$lib/flow/utils';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { logIn, user } from '$lib/flow/utils';
 
-  onMount(() => {
-    if ($user.loggedIn) {
-      goto('/home');
-    }
-  });
+	onMount(() => {
+		if ($user.loggedIn) {
+			goto('/home');
+		}
+	});
 </script>
 
 <svelte:head>
-  <title>Login</title>
-  <meta name="description" content="Login page" />
+	<title>Login</title>
+	<meta name="description" content="Login page" />
 </svelte:head>
 
-<style>
-  .login-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #000080; /* dark blue */
-  }
-
-  .login-box {
-    width: 400px;
-    padding: 2em;
-    background: white;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    border-radius: 15px;
-    text-align: center;
-    box-sizing: border-box;
-  }
-
-  img {
-    width: 100%;
-    margin-bottom: 1em;
-  }
-
-  .buttons button {
-    width: 100%;
-    padding: 10px 0;
-    margin-top: 1em;
-  }
-</style>
-
 <div class="login-container">
-  <div class="login-box">
-    <img src="logo-words.png" alt="logo" />
-    <h2>Please log in to continue</h2>
-    <div class="buttons">
-      <button on:click={logIn}>Log In</button>
-    </div>
-  </div>
+	<div class="login-box">
+		<img src="logo-words.png" alt="logo" />
+		<h2>Please log in to continue</h2>
+		<div class="buttons">
+			<button on:click={logIn}>Log In</button>
+		</div>
+	</div>
 </div>
