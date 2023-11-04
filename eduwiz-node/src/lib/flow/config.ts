24e175731640d78c020b93d3dd0f56807ae74fd6
Anton Angeletti <angelettianton@gmail.com>
2023-11-04 16:34:03 -0500

    rust weird websockets bad

--- eduwiz-node/src/lib/flow/config.ts
+++ eduwiz-node/src/lib/flow/config.ts
@@ -3,7 +3,7 @@ import { config } from '@onflow/fcl'
 config({
   'app.detail.title': 'EduWiz',
   'accessNode.api': 'https://rest-testnet.onflow.org',
-  'app.detail.icon': 'https://imgs.search.brave.com/Tch9yMPmKQUxcKh-OkYHb2WzBiu4TZw6KshFkVEAsq0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9ibG9n/LmxvZ29teXdheS5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMTIvcmVkZGl0/LWxvZ28tYXBwLmpw/Zw',
+  'app.detail.icon': 'https://i.imgur.com/zNsSyWP.png',
   'discovery.wallet': 'https://fcl-discovery.onflow.org/testnet/authn',
   'flow.network': 'testnet',
   'profileContractAddress': '0x6e6efd2c0e2ad3c3',
