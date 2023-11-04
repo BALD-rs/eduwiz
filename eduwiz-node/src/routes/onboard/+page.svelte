268c779f0aa5852551d69b71b246e7aa0f22bafe
Blaine Traudt <blaine-t@bathost.net>
2023-11-04 16:46:51 -0500

    Updating username code

--- eduwiz-node/src/routes/onboard/+page.svelte
+++ eduwiz-node/src/routes/onboard/+page.svelte
@@ -1,4 +1,17 @@
-<script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { setUsername } from '$lib/flow/utils';
+	import { writable } from 'svelte/store';
+
+	const loading = writable(false);
+
+	async function updateUsername() {
+		const inputElement = document.querySelector('input#username') as HTMLInputElement;
+		loading.set(true);
+		await setUsername(inputElement?.value);
+		loading.set(false);
+		goto('/home');
+	}
 </script>
 
 <svelte:head>
@@ -8,10 +21,35 @@
 
 <div class="login-container">
 	<div class="username-panel">
-		<h1>Set a username</h1>
-		<form class="internal-panel" method="POST">
-			<input type="text" name="username" />
-			<button type="submit">Submit</button>
-		</form>
+		{#if $loading}
+			<h1>Updating username...</h1>
+			<div class="spinner" />
+		{:else}
+			<h1>Set a username</h1>
+			<form class="internal-panel">
+				<input type="text" name="username" id="username" />
+				<button on:click={async () => updateUsername()}>Submit</button>
+			</form>
+		{/if}
 	</div>
 </div>
+
+<style>
+	.spinner {
+		width: 40px;
+		height: 40px;
+		border: 4px solid #f3f3f3;
+		border-top: 4px solid #3498db;
+		border-radius: 50%;
+		animation: spin 2s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
